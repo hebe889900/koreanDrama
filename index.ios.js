09126656e96bb196dcb8ebb0212e1680919c9b24
jackyan63@gmail.com <jackyan63@gmail.com>
2016-09-30 17:45:58 -0400

    Turn ES5 syntax into ES6 syntax.

--- index.ios.js
+++ index.ios.js
@@ -9,45 +9,105 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  ScrollView,
+  NavigatorIOS,
+  TabBarIOS
 } from 'react-native';
 
-class koreanDrama extends Component {
+import ShotList from "./app/ShotList";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+export default class koreanDrama extends Component {
+
+  constructor() {
+        super();
+        this.state = {
+            selectedTab: "default"
+        }
+    }
+  
+  _renderContent(category: string, title: ?string) {
+    return (
+      <NavigatorIOS style={styles.wrapper}
+        initialRoute={{
+          component: ShotList,
+          title: title,
+          passProps: {filter: category}
+        }}
+      />
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <TabBarIOS tintColor={"#ea4c89"}>
+        <Icon.TabBarItem
+          title="All"
+          iconName="dribbble"
+          selectedIconName="dribbble"
+          selected={this.state.selectedTab === "default"}
+          onPress={() => {
+            this.setState({
+              selectedTab: "default",
+            });
+          }}>
+          {this._renderContent("default", "All")}
+        </Icon.TabBarItem>
+        <Icon.TabBarItem
+          title="Debuts"
+          iconName="trophy"
+          selectedIconName="trophy"
+          selected={this.state.selectedTab === "debuts"}
+          onPress={() => {
+            this.setState({
+              selectedTab: "debuts",
+            });
+          }}>
+          {this._renderContent("debuts", "Debuts")}
+        </Icon.TabBarItem>
+        <Icon.TabBarItem
+          title="Animated"
+          iconName="heart"
+          selectedIconName="heart"
+          selected={this.state.selectedTab === "animated"}
+          onPress={() => {
+            this.setState({
+              selectedTab: "animated",
+            });
+          }}>
+          {this._renderContent("animated", "Animated")}
+        </Icon.TabBarItem>
+        <Icon.TabBarItem
+          title="Rebounds"
+          iconName="lightbulb-o"
+          selectedIconName="lightbulb-o"
+          selected={this.state.selectedTab === "rebounds"}
+          onPress={() => {
+            this.setState({
+              selectedTab: "rebounds",
+            });
+          }}>
+          {this._renderContent("rebounds", "Rebounds")}
+        </Icon.TabBarItem>
+      </TabBarIOS>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  tabContent: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: "center",
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  tabText: {
+    color: "white",
+    margin: 50,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  wrapper: {
+    flex: 1
+  }
 });
 
 AppRegistry.registerComponent('koreanDrama', () => koreanDrama);
--- ios/koreanDrama/AppDelegate.m
+++ ios/koreanDrama/AppDelegate.m
@@ -17,15 +17,15 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-
+  
+    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"koreanDrama"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
